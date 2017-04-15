d8ab667bbb840c039bae33d9a9672e246d235f95
jayven <kassarar@gmail.com>
2017-04-15 09:44:35 +0800

    Add some meta data.

---
+++ tagcache/__init__.py
@@ -0,0 +1,11 @@
+# -*- encoding: utf-8 -*-
+
+from tagcache import metadata
+
+__version__ = metadata.version
+
+__author__ = metadata.authors[0]
+
+__license__ = metadata.license
+
+__copyright__ = metadata.copyright
