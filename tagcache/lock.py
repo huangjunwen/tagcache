a4b6caf3a524d6da7994588f89a4f98d718bc933
jayven <kassarar@gmail.com>
2017-04-15 10:49:19 +0800

    Make the lock code even simpler.

--- tagcache/lock.py
+++ tagcache/lock.py
@@ -6,27 +6,15 @@ import fcntl
 
 class FileLock(object):
 
-    def __init__(self, path):
+    def __init__(self, fd):
 
-        self.path = path
+        # the fd is borrowed, so do not close it
+        self.fd = fd
 
-        self.fd = None
-
-    def acquire(self, write=False, block=True):
-
-        if self.fd is not None:
-
-            self.release()
+    def acquire(self, write=False, block=False):
 
         try:
-            # open or create the file
-            open_flags = os.O_RDWR if write else os.O_RDONLY
 
-            open_flags |= os.O_CREAT
-
-            self.fd = os.open(self.path, open_flags)
-
-            # try to lock the file
             lock_flags = fcntl.LOCK_EX if write else fcntl.LOCK_SH
 
             if not block:
@@ -35,29 +23,15 @@ class FileLock(object):
 
             fcntl.flock(self.fd, lock_flags)
 
-            return self.fd
+            return True
 
-        #except (OSError, IOError):
-        except:
-
-            # open file failed or lock failed
-            if self.fd is not None:
-
-                os.close(self.fd)
-
-                self.fd = None
-
-        return None
+        except IOError:
+            
+            return False
 
     def release(self):
 
-        if self.fd is None:
-
-            return
-
-        os.close(self.fd)
-
-        self.fd = None
+        fcntl.flock(self.fd, fcntl.LOCK_UN)
 
 
 
